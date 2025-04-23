a478d8315fe796cc9c3374b1c0199747e3ee0b32
Humberto Torres <133621997+H05T3@users.noreply.github.com>
2025-04-23 07:43:51 +0000

    map class test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+
+class Map {
+
+	vector<vector<string>> grid;
+	public:
+		int rows;
+		int cols;
+		//R = Row; C = collumn
+		Map(int R=15, int C=15){
+			rows = R;
+			cols = C;
+			grid.resize(rows);
+			for (int r = 0; r < rows; r++){
+				grid[r].resize(cols);
+				for( int c = 0; c<cols; c++){
+					grid[r][c] = ".";
+				}
+			}
+		}
+		void display(){
+			cout << "   ";
+			for(int c=0; c<cols; c++){
+				cout << c << " ";
+			}
+			for(int r= 0;r<rows;r++){
+				cout<< r << " ";
+				for (int c=0; c<cols;c++){
+					cout<<grid[r][c]<< " ";}
+			}
+		}
+		bool isInBounds(int r, int c){
+			return (r>=0 && r<rows && c >=0 && c<cols);
+		}
+		bool isEmpty(int r, int c){
+			if (!isInBounds(r,c))
+				return false;
+			return (gric[r][c] == ".");
+		}
+		bool place(int r, int c, string &tok){
+			if(isEmpty(r,c)){
+				grid[r][c] = tok;
+				return true;
+			}
+			return false;
+		}
+		void clear(int r, int c) {
+			if(isInBounds(r,c)){
+				grid[r][c] = ".";
+			}
+		}
+
+};
 
 int main(){
-	//Map m(15,15)
+	Map m;
+	m.display();
 	//Player P1;
 	//Player P2;
 	int player Turn 1;
